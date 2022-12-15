f6b568a65f3c361fb6f31fb689dd0cd4cb774b18
Kundan Kumar Kushwaha <starkkumarkk1611@gmail.com>
2022-12-15 12:14:52 +0530

    fine

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const csrf = require("csurf");
 const bodyParser = require("body-parser");
 const express = require("express");
 const admin = require("firebase-admin");
+require('dotenv').config()
 
 const path = require("path");
 const { start } = require("repl");
@@ -23,8 +24,8 @@ const app = express();
 
 
 app.use('/static', express.static('static')) //for seving static files
-
-app.use(express.urlencoded());//for getting filled data in form
+app.use(express.urlencoded({ extended: true }))
+// app.use(express.urlencoded());//for getting filled data in form
 
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -33,7 +34,7 @@ app.use(csrfMiddleware);
 
 //PUG STUFFS
 app.set('view engine', 'pug');// setting template engine as pug
-app.set('views',path.join(__dirname, 'views'));//set the views directory 
+app.set('views', path.join(__dirname, 'views'));//set the views directory 
 
 //new
 app.all("*", (req, res, next) => {
@@ -42,34 +43,31 @@ app.all("*", (req, res, next) => {
 });
 //new
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
 
-    const tit = "ISMpetition";
-    const parmas = {'title':tit};
-    res.status(200).render('index',parmas);
+  const tit = "ISMpetition";
+  const parmas = { 'title': tit };
+  res.status(200).render('index', parmas);
 });
 
 
 
 
-app.get('/home', (req,res)=>{
+app.get('/home', (req, res) => {
   const sessionCookie = req.cookies.session || "";
   admin
-  .auth()
-  .verifySessionCookie(sessionCookie, true /** checkRevoked */)
-  .then((user) => {
-    const tit = "ISMpetition | Home";
-    const parmas = {'title':tit,'email':user.email};
-    res.status(200).render('MainPage',parmas);
-  })
-  .catch((error) => {
-    res.redirect("/signin-signup");
-  });
- 
+    .auth()
+    .verifySessionCookie(sessionCookie, true /** checkRevoked */)
+    .then((user) => {
+      const tit = "ISMpetition | Home";
+      const parmas = { 'title': tit, 'email': user.email };
+      res.status(200).render('MainPage', parmas);
+    })
+    .catch((error) => {
+      res.redirect("/signin-signup");
+    });
+
 });
-
-
-
 
 
 app.post("/sessionLogin", (req, res) => {
@@ -97,63 +95,63 @@ app.get("/sessionLogout", (req, res) => {
   res.redirect("/");
 });
 
-   
+
 //for redirecting to sign in page
-app.get('/signin-signup', (req,res)=>{
+app.get('/signin-signup', (req, res) => {
   const tit = "ISMpetition | Sign In - Sign Up";
-  const parmas = {'title':tit};
-  res.status(200).render('sign_in_sign_up',parmas);
+  const parmas = { 'title': tit };
+  res.status(200).render('sign_in_sign_up', parmas);
 });
 
 
 
 //start-a-petition 
-app.get('/start-a-petition', (req,res)=>{
+app.get('/start-a-petition', (req, res) => {
   const sessionCookie = req.cookies.session || "";
   admin
-  .auth()
-  .verifySessionCookie(sessionCookie, true /** checkRevoked */)
-  .then((user) => {
-    const tit = "Start Your Petition";
-    const parmas = {'title':tit,'email':user.email};
-    res.status(200).render('start_petition',parmas);
-  })
-  .catch((error) => {
-    res.redirect("/signin-signup");
-  });
- 
-  
+    .auth()
+    .verifySessionCookie(sessionCookie, true /** checkRevoked */)
+    .then((user) => {
+      const tit = "Start Your Petition";
+      const parmas = { 'title': tit, 'email': user.email };
+      res.status(200).render('start_petition', parmas);
+    })
+    .catch((error) => {
+      res.redirect("/signin-signup");
+    });
+
+
 });
 
-app.get('/preview', (req,res)=>{
+app.get('/preview', (req, res) => {
   const sessionCookie = req.cookies.session || "";
   admin
-  .auth()
-  .verifySessionCookie(sessionCookie, true /** checkRevoked */)
-  .then((user) => {
-    const Petition = url.parse(req.url,true).query;
-    console.log(Petition);
-    Petition.email = user.email;
-    res.status(200).render('preview',Petition);
-  })
-  .catch((error) => {
-    res.redirect("/signin-signup");
-  });
+    .auth()
+    .verifySessionCookie(sessionCookie, true /** checkRevoked */)
+    .then((user) => {
+      const Petition = url.parse(req.url, true).query;
+      console.log(Petition);
+      Petition.email = user.email;
+      res.status(200).render('preview', Petition);
+    })
+    .catch((error) => {
+      res.redirect("/signin-signup");
+    });
 });
 
-app.get('/petition', (req,res)=>{
-  const queryObject = url.parse(req.url,true).query; 
+app.get('/petition', (req, res) => {
+  const queryObject = url.parse(req.url, true).query;
   const id = queryObject.id;
-  var parmas = {"id":id};
-  res.status(200).render('petition',parmas);
- 
+  var parmas = { "id": id };
+  res.status(200).render('petition', parmas);
+
 });
 
- 
 
 
 
-app.listen( PORT, ()=>{
-    console.log("the application started sucessfully");
 
-} )
+app.listen(PORT, () => {
+  console.log("the application started sucessfully");
+
+})
--- static/firebaseConfig.js
+++ static/firebaseConfig.js
@@ -1,13 +1,14 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyA1zibnPZl4ocIwVHMDFg3Hs7-VzXjDtSU",
-    authDomain: "ismpetition-c42af.firebaseapp.com",
-    projectId: "ismpetition-c42af",
-    storageBucket: "ismpetition-c42af.appspot.com",
-    messagingSenderId: "523773171656",
-    appId: "1:523773171656:web:790c1724b9ff0f854012fd",
-    measurementId: "G-WM9JHM9N7Y"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  console.log("fireConfig")
-  var db = firebase.firestore();
+const firebaseConfig = {
+  apiKey: "AIzaSyBjHnipVvpPJfhQqy_NGuWtbEaZ9YyKjaY",
+  authDomain: "ismpetition-c42af.firebaseapp.com",
+  databaseURL: "https://ismpetition-c42af-default-rtdb.firebaseio.com",
+  projectId: "ismpetition-c42af",
+  storageBucket: "ismpetition-c42af.appspot.com",
+  messagingSenderId: "523773171656",
+  appId: "1:523773171656:web:790c1724b9ff0f854012fd",
+  measurementId: "G-WM9JHM9N7Y"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+console.log("fireConfig")
+var db = firebase.firestore();
